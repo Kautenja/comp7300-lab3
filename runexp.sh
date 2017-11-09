704ba2683442eb09483f5338fbb0df8e2db13e5a
Kautenja <ckfour@icloud.com>
2017-11-09 14:38:02 -0600

    asdf

---
+++ runexp.sh
@@ -0,0 +1,13 @@
+mkdir -p ./build
+echo 'compiling'
+cc $1.c -o ./build/$1
+echo 'running 1'
+./build/$1 > ./build/$1.out
+echo 'running 2'
+./build/$1 >> ./build/$1.out
+echo 'running 3'
+./build/$1 >> ./build/$1.out
+echo 'running 4'
+./build/$1 >> ./build/$1.out
+echo 'running 5'
+./build/$1 >> ./build/$1.out
